91ef82bf23cbd032fd0ff049268e0aebe89b1a8e
Ayomide <ayomidet905@gmail.com>
2026-09-06 19:20:03 +0100

    Changed base model; increased epochs to 20

--- finetune/config.py
+++ finetune/config.py
@@ -35,7 +35,7 @@ peft_config = LoraConfig(
 
 sft_config = SFTConfig(
     output_dir='checkpoints',
-    num_train_epochs=3,
+    num_train_epochs=10,
     per_device_train_batch_size=4,
     per_device_eval_batch_size=4,
     gradient_accumulation_steps=4,
